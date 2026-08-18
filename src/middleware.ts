05d21bde765d49545c1b52f5c2b7f65384224329
EGABO AARON <aaronegabo@gmail.com>
2026-08-18 14:40:16 +0300

    Removed favicon.io from sitemap redirect error

--- src/middleware.ts
+++ src/middleware.ts
@@ -82,6 +82,6 @@ export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
-    '/((?!_next/static|_next/image|favicon.ico|sitemap\\.xml|robots\\.txt).*)',
+    '/((?!_next/static|_next/image|sitemap\\.xml|robots\\.txt).*)',
   ],
 };
